627f66bad087fb7a40de09dd84352a869ed1677c
Gregorcyk <tyler.gregorcyk@kestrafinancial.com>
2024-03-08 11:44:01 -0600

    cleanup

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,6 +3,7 @@ oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/
 Set-Alias lvim 'C:\Users\tyler.gregorcyk\.local\bin\lvim.ps1'
 
 Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })
+Set-Alias -Name lg -Value lazygit
 
 <#
 Fuzzy search Settings
@@ -14,6 +15,14 @@ Set-PsFzfOption -EnableAliasFuzzyKillProcess
 Set-PsFzfOption -EnableAliasFuzzyHistory
 Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
 
+
+Set-Alias -Name cda -Value FindAllDirectoriesWithFzf
+Set-Alias -Name cdf -Value ChangeDirectoryWithFzf
+Set-Alias -Name cdu -Value FindAllUserDirectoryWithFzf
+
+
+# Functions
+# :
 function FindAllDirectoriesWithFzf
 {
   Get-ChildItem / -Recurse -Attributes Directory -ErrorAction SilentlyContinue | Invoke-Fzf | Set-Location
@@ -22,7 +31,7 @@ function ChangeDirectoryWithFzf
 {
   Get-ChildItem . -Recurse -Attributes Directory -ErrorAction SilentlyContinue | Invoke-Fzf | Set-Location
 }
-
-Set-Alias -Name cda -Value FindAllDirectoriesWithFzf
-Set-Alias -Name cdf -Value ChangeDirectoryWithFzf
-
+function FindAllUserDirectoryWithFzf
+{
+  Get-ChildItem ~/ -Recurse -Attributes Directory -ErrorAction SilentlyContinue | Invoke-Fzf | Set-Location
+}
