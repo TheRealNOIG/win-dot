a4b275bd097b8a72c399cda9679e9a6fc1fc9d19
Gregorcyk <tyler.gregorcyk@kestrafinancial.com>
2024-03-08 10:59:34 -0600

    added PSFzf confige to powershell config

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,3 +3,19 @@ oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/
 Set-Alias lvim 'C:\Users\tyler.gregorcyk\.local\bin\lvim.ps1'
 
 Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })
+
+<#
+Fuzzy search Settings
+#>
+iex "$(thefuck --alias)"
+Set-PsFzfOption -EnableAliasFuzzyEdit
+Set-PsFzfOption -EnableAliasFuzzySetLocation
+Set-PsFzfOption -EnableAliasFuzzyKillProcess
+Set-PsFzfOption -EnableAliasFuzzyHistory
+Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
+
+function ChangeDirectoryWithFzf {
+    Get-ChildItem . -Recurse -Attributes Directory | Invoke-Fzf | Set-Location
+}
+Set-Alias -Name cda -Value ChangeDirectoryWithFzf
+
