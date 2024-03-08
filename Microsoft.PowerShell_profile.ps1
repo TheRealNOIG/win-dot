e1e757d2aa07c55037bdf887627b578fa2a4f9f6
Gregorcyk <tyler.gregorcyk@kestrafinancial.com>
2024-03-08 11:46:59 -0600

    more cleanup

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,15 @@
 oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/night-owl.omp.json' | Invoke-Expression
 
+# ---------------------------------------------------------------------------------------------------------
+# Aliases
+# ---------------------------------------------------------------------------------------------------------
 Set-Alias lvim 'C:\Users\tyler.gregorcyk\.local\bin\lvim.ps1'
-
-Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })
 Set-Alias -Name lg -Value lazygit
+Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })
 
-<#
-Fuzzy search Settings
-#>
+# ---------------------------------------------------------------------------------------------------------
+# Fuzzy search Settings
+# ---------------------------------------------------------------------------------------------------------
 Invoke-Expression "$(thefuck --alias)"
 Set-PsFzfOption -EnableAliasFuzzyEdit
 Set-PsFzfOption -EnableAliasFuzzySetLocation
@@ -15,14 +17,17 @@ Set-PsFzfOption -EnableAliasFuzzyKillProcess
 Set-PsFzfOption -EnableAliasFuzzyHistory
 Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
 
-
+# ---------------------------------------------------------------------------------------------------------
+# Fuzzy CD Aliases
+# ---------------------------------------------------------------------------------------------------------
 Set-Alias -Name cda -Value FindAllDirectoriesWithFzf
 Set-Alias -Name cdf -Value ChangeDirectoryWithFzf
 Set-Alias -Name cdu -Value FindAllUserDirectoryWithFzf
 
 
+# ---------------------------------------------------------------------------------------------------------
 # Functions
-# :
+# ---------------------------------------------------------------------------------------------------------
 function FindAllDirectoriesWithFzf
 {
   Get-ChildItem / -Recurse -Attributes Directory -ErrorAction SilentlyContinue | Invoke-Fzf | Set-Location
